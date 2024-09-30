52ec2458d48569753ad1ae6ff4eca5aeccf72eae
maltafdhiyaurrahman <altafff02@gmail.com>
2024-09-30 21:21:41 +0700

    fix index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router'; // Ganti dari createWebHistory
 import HomeViews from '../views/HomeViews.vue';
 import LoginViews from '../views/LoginViews.vue';
 
@@ -15,7 +15,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), // Ganti ke createWebHashHistory
   routes,
 });
 
